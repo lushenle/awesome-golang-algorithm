ebfb3a7e695b9d18d876e66586d40328e16c5fdc
lushenle <lushenle@gmail.com>
2022-03-16 14:56:32 +0800

    add comments for bubble sort

--- sorts/bubble_sort.go
+++ sorts/bubble_sort.go
@@ -5,8 +5,11 @@ func BubbleSort(nums []int) []int {
 
 	for swapped {
 		swapped = false
+		// `length -i - 1` is for ignoring comparisons of elements which have
+		// already been compared in earlier iterations
 		for i := 0; i < len(nums)-1; i++ {
 			if nums[i+1] < nums[i] {
+				// here swapping of positions is being done
 				nums[i+1], nums[i] = nums[i], nums[i+1]
 				swapped = true
 			}
